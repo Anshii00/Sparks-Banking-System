6e83fecf30feca5569502efe1e78ca0863d3e707
Anshii00 <87702529+Anshii00@users.noreply.github.com>
2021-08-19 12:18:45 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "anshita";
+$password = "";
+$db = "anshi";
+//Conneting to db
+$conn = mysqli_connect($servername, $username, $password, $db);
+if(!$conn)
+{
+    die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+}
+?>
